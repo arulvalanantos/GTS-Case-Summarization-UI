ab8a36ef01b3c0d24df859fe32b9442c3c27b71c
arulvalananto <arulvalananto@gmail.com>
2025-03-18 14:57:06 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return (
+        <main>
+            <div>Case Summary</div>
+            <div>Case Notes</div>
+        </main>
+    )
+}
+
+export default App
